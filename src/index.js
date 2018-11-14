3ee466ba18168d193913b4e378b3479dcac09d21
Preston Wallace <wallace.preston@gmail.com>
2018-11-13 16:40:27 -0800

    added pretty map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 console.log("Hello from JavaScript");
+
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoicHJlc3RvbndhbGxhY2UiLCJhIjoiY2pvZ2ZicTdyMDRqcjN2bGJrN2p4b2oxciJ9.S-kMjDjADAbBxEJ0j_W1BQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+markerDomEl.style.width = "32px";
+markerDomEl.style.height = "39px";
+markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.641, 41.895] for Chicago
