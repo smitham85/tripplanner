723fd6c7f3a3c7284e63e400c126db792bee2468
Ashley Smith <smitham85@gmail.com>
2018-11-13 16:51:17 -0800

    ashley v2

---
+++ src/marker.js
@@ -0,0 +1,13 @@
+const mapbox = require('mapbox-gl');
+
+const iconURLs = {
+  hotels: 'http://i.imgur.com/D9574Cu.png',
+  restaurants: 'http://i.imgur.com/cqR6pUI.png',
+  activities: 'http://i.imgur.com/WbMOfMl.png',
+};
+
+const buildMarker = function(type, coords) {
+  // Your Code Here
+};
+
+module.exports = buildMarker;
